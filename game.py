cce8a20a8b33c40daab65a1a47ddd97f80312a25
tenznz <dhugkar@gmail.com>
2022-01-25 02:53:53 -0800

    Added: docstrings

--- game.py
+++ game.py
@@ -4,31 +4,43 @@ from random import randint
 class SnakeLadder:
 
     def __init__(self):
-        self.ladder_dict = {}
-        self.snake_dict = {}
-
-    def dice_rolled(self):
-        return randint(1, 6)
-
-    def snake(self, position):
         self.snake_dict = {
             98: 1,
             52: 20,
             87: 12,
             20: 3
         }
-        if position in self.snake_dict:
-
-            position = position - self.snake_dict.get(position)
-        return position
-
-    def ladder(self, position):
         self.ladder_dict = {
             20: 12,
             12: 40,
             92: 1,
             18: 23
         }
+
+    def dice_rolled(self):
+        """
+        dice roll 1 to 6
+        :return: number from 1 to 6
+        """
+        return randint(1, 6)
+
+    def snake(self, position):
+        """
+        Subtract player position
+        :param position:player position
+        :return: player position
+        """
+
+        if position in self.snake_dict:
+            position = position - self.snake_dict.get(position)
+        return position
+
+    def ladder(self, position):
+        """
+        Added position
+        :param position:player position
+        :return: player position
+        """
         if position in self.ladder_dict:
 
             position = position + self.ladder_dict.get(position)
--- game_test.py
+++ game_test.py
@@ -2,15 +2,27 @@ from snake_ladder.game import SnakeLadder
 
 
 def test_dice_rolled():
+    """
+    test dice range
+    :return:
+    """
     snake_ladder = SnakeLadder()
     assert snake_ladder.dice_rolled() in range(1, 6)
 
 
 def test_snake():
+    """
+    test snake func
+    :return:
+    """
     snake_ladder = SnakeLadder()
     assert snake_ladder.snake(98) == 97
 
 
 def test_ladder():
+    """
+    test ladder func
+    :return:
+    """
     snake_ladder = SnakeLadder()
     assert snake_ladder.ladder(18) == 41
